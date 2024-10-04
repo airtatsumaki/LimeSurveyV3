b856893cfefa6e82135e34926f3d4c27b8d39de7
airtatsumaki <naz1mb20@gmail.com>
2024-10-04 12:18:12 +0100

    test run

--- lime-functions.js
+++ lime-functions.js
@@ -1,13 +1,14 @@
 function fixToBottomInOrder(qid, itemsToFix) {
-    var thisQuestion = $("#question" + qid);
-    $.each(itemsToFix, function (index, answer) {
-        if ($(thisQuestion).hasClass("list-radio")) {
-            $(".answer-item[id$=X" + qid + answer + "]", thisQuestion).appendTo(
-                $(
-                    ".answer-item[id$=X" + qid + answer + "]",
-                    thisQuestion
-                ).parent()
-            );
-        }
-    });
+    console.log("you called fixToBottomInOrder");
+    // var thisQuestion = $("#question" + qid);
+    // $.each(itemsToFix, function (index, answer) {
+    //     if ($(thisQuestion).hasClass("list-radio")) {
+    //         $(".answer-item[id$=X" + qid + answer + "]", thisQuestion).appendTo(
+    //             $(
+    //                 ".answer-item[id$=X" + qid + answer + "]",
+    //                 thisQuestion
+    //             ).parent()
+    //         );
+    //     }
+    // });
 }
