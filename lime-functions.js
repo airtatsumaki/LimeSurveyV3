6ea9d75b01024531bb88efcd04efedbcafb3c26a
airtatsumaki <naz1mb20@gmail.com>
2024-10-14 10:58:47 +0100

    uncommented

--- lime-functions.js
+++ lime-functions.js
@@ -1,14 +1,14 @@
 function fixToBottomInOrder(qid, itemsToFix) {
     console.log("you called fixToBottomInOrder");
-    // var thisQuestion = $("#question" + qid);
-    // $.each(itemsToFix, function (index, answer) {
-    //     if ($(thisQuestion).hasClass("list-radio")) {
-    //         $(".answer-item[id$=X" + qid + answer + "]", thisQuestion).appendTo(
-    //             $(
-    //                 ".answer-item[id$=X" + qid + answer + "]",
-    //                 thisQuestion
-    //             ).parent()
-    //         );
-    //     }
-    // });
+    var thisQuestion = $("#question" + qid);
+    $.each(itemsToFix, function (index, answer) {
+        if ($(thisQuestion).hasClass("list-radio")) {
+            $(".answer-item[id$=X" + qid + answer + "]", thisQuestion).appendTo(
+                $(
+                    ".answer-item[id$=X" + qid + answer + "]",
+                    thisQuestion
+                ).parent()
+            );
+        }
+    });
 }
